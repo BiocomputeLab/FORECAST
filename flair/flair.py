b0c6d9218e8fc406894cfea7e36c4b9a1a73dca5
Pierre-Aurelien Gilliot <66125433+Pierre-Aurelien@users.noreply.github.com>
2020-10-27 16:06:48 +0000

    compartmentalised inference and simulations into two modules

--- flair/flair.py
+++ flair/flair.py
@@ -1,87 +1,84 @@
 import numpy as np
 import pandas as pd
-import random
+#import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy as sc
-import scipy.stats as stats 
+import scipy.stats as stats
 from scipy.special import factorial,digamma
 import numdifftools as nd
-from scipy.optimize import minimize 
+from scipy.optimize import minimize
 from joblib import Parallel, delayed
 
 
 ##############################################################################################################
 ###########################################  Functions   #####################################################
 
-def intensity_parameter_reparameterisation(i,j,alpha,beta):  # We enforced the positive constraint on a and b by rewriting alpha=log(a) and beta=log(b)
-    Number_construct=Ni[i]
-    if Nj[j]==0:
-        return(0)
-    else :
-        probability_bin=stats.norm.cdf(Part_conv[j+1],loc=np.exp(alpha),scale=np.exp(beta))-stats.norm.cdf(Part_conv[j],loc=np.exp(alpha),scale=np.exp(beta))
-        return Number_construct*probability_bin*READS[j]/Nj[j]
 
-def data_transformation_bins(X):  #New representation of the data enabling the method of moments
-    X=np.ceil(X)
-    X=X.astype(int)
-    T=np.repeat(Mean_expression_bins,X)
-    return(T)
-
-
-def starting_point_binned_reparameterised(X):   #Compute empirical moments from data and return the log reparameterisation of both shape and scale parameters of the gamma distribution 
-    X=np.ceil(X)
-    X=X.astype(int)
-    T=data_transformation_bins(X)
-    if np.count_nonzero(X)==1:  #What if all the cells fall into one unique bin?
-        j=np.where(X!=0)[0][0]
-        mu=np.mean(T)
+def starting_point(i,FLUORESCENCE_MAX,BINS,Nj,READS,Nijhat,Nihat,distribution,Mean_expression_bins,Part_conv):
+#Takes as input the construct number i
+#Returns the empirical mean and standard deviation
+    T=np.ceil(Nijhat[i,:]).astype(int)
+    T=np.repeat(Mean_expression_bins,T)
+    if np.max(T) == np.min(T):  #What if all the cells fall into one unique bin?
+        j=np.where(Mean_expression_bins==np.max(T))[0][0]
+        mu=np.max(T)
         std=(Part_conv[j+1]-Part_conv[j])/4
     elif not np.any(T):
         return(np.array([0,0]))
     else:
         mu=np.mean(T)
         std=np.std(T,ddof=1)
-    return np.log(np.array([mu,std]))
+    return (np.array([mu,std]))
 
 
-def neg_ll_rep(theta,construct):
+def neg_ll_rep(theta,i,BINS,Part_conv,READS,Nj,Nihat,distribution,Sij):
+#takes as input the parameter theta=(alpha,beta), the construct number i
+#Returns the likelihood
     alpha=theta[0]
     beta=theta[1]
     NL=0
-    i=construct
     for j in range(BINS):
-        intensity=intensity_parameter_reparameterisation(i,j,alpha,beta)
-        if Sij[construct,j]!=0:
+    #Compute intensity parameter
+        if Nj[j]==0:
+            intensity=0
+        else :
+            if distribution=='lognormal':
+                probability_bin=stats.norm.cdf(Part_conv[j+1],loc=np.exp(alpha),scale=np.exp(beta))-stats.norm.cdf(Part_conv[j],loc=np.exp(alpha),scale=np.exp(beta))
+            else:
+                probability_bin=stats.gamma.cdf(Part_conv[j+1],loc=np.exp(alpha),scale=np.exp(beta))-stats.gamma.cdf(Part_conv[j],loc=np.exp(alpha),scale=np.exp(beta))
+            intensity=Nihat[i]*probability_bin*READS[j]/Nj[j]
+    #Compute Likelihood
+        if Sij[i,j]!=0:
             if intensity>0: #Avoid float error with np.log
                 NL+=intensity-Sij[i,j]*np.log(intensity)
         else:
             NL+=intensity
     return(NL)
 
-def is_pos_def(X):
-    return np.all(np.linalg.eigvals(X) > 0)
 
-def ML_inference_reparameterised(i):
+def ML_inference_reparameterised(i,FLUORESCENCE_MAX,BINS,Nj,READS,Nijhat,Nihat,distribution,Mean_expression_bins,Part_conv,Sij):
+#Takes as input the construct number i
+#Returns a numpy array containing the FLAIR inference,confidence intervals, MOM inference, scoring and validity of ML inference
     Dataresults=np.zeros(8)
-    T=Nihat[i,:]
+    T=Nijhat[i,:]
     if np.sum(T)!=0:     #Can we do inference? has the genetic construct been sequenced?
         Dataresults[7]=(T[0]+T[-1])/np.sum(T) #Scoring of the data- How lopsided is the read count? all on the left-right border?
-        a,b=starting_point_binned_reparameterised(T)
+        SP=starting_point(i,FLUORESCENCE_MAX,BINS,Nj,READS,Nijhat,Nihat,distribution,Mean_expression_bins,Part_conv)
         #The four next lines provide the MOM estimates on a,b, mu and sigma
-        Dataresults[4]=np.exp(a) #value of mu MOM
-        Dataresults[5]=np.exp(b) #Value of sigma MOM
+        Dataresults[4]=SP[0] #value of mu MOM
+        Dataresults[5]=SP[1] #Value of sigma MOM
         if np.count_nonzero(T)==1: #is there only one bin to be considered? then naive inference
             Dataresults[6]=3 #Inference grade 3 : Naive inference
         else:  #in the remaining case, we can deploy the mle framework to improve the mom estimation
-            res=minimize(neg_ll_rep,starting_point_binned_reparameterised(T),args=(i),method="Nelder-Mead")
+            res=minimize(neg_ll_rep,np.log(SP),args=(i,BINS,Part_conv,READS,Nj,Nihat,distribution,Sij),method="Nelder-Mead")
             c,d=res.x
             Dataresults[0]=np.exp(c) #value of mu, MLE
             Dataresults[1]=np.exp(d) #value of sigma squared, MLE
-            fi = lambda x: neg_ll_rep(x,i)
-            fdd = nd.Hessian(fi) 
+            fi = lambda x: neg_ll_rep(x,i,BINS,Part_conv,READS,Nj,Nihat,distribution,Sij)
+            fdd = nd.Hessian(fi)
             hessian_ndt=fdd([c, d])
-            if is_pos_def(hessian_ndt)==True:
+            if np.all(np.linalg.eigvals(hessian_ndt))==True:
                 inv_J=np.linalg.inv(hessian_ndt)
                 e,f=np.sqrt(np.diag(np.matmul(np.matmul(np.diag((np.exp(c),np.exp(d))),inv_J),np.diag((np.exp(c),np.exp(d))))))
                 Dataresults[2]=e
@@ -92,5 +89,13 @@ def ML_inference_reparameterised(i):
     else:
         Dataresults[6]=4   #Inference grade 4: No inference is possible
     return(Dataresults)
+
+
+def inference(p,FLUORESCENCE_MAX,BINS,Nj,READS,Nijhat,Nihat,distribution,Mean_expression_bins,Part_conv,Sij):
+    Data_results = Parallel(n_jobs=-1,max_nbytes=None)(delayed(ML_inference_reparameterised)(i,FLUORESCENCE_MAX,BINS,Nj,READS,Nijhat,Nihat,distribution,Mean_expression_bins,Part_conv,Sij)for i in range(p))
+    Data_results=np.array(Data_results)
+    df= pd.DataFrame(Data_results)
+    df.rename(columns={0: "mu_MLE", 1: "sigma_MLE", 2: "mu_std",3: "sigma_std",4: "mu_MOM", 5: "sigma_MOM", 6: "Inference_grade",7: "Score"}, errors="raise",inplace=True)
+    print(df.head())
 
 
---
+++ flair/simulations.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sc
+import scipy.stats as stats
+from scipy.special import gamma, factorial,digamma
+import numdifftools as nd
+from scipy.optimize import minimize
+from joblib import Parallel, delayed
+
+
+
+def Sorting_and_Sequencing(BINS,Diversity,N,BUDGET_READS,Ratio_amplification,BIAS_LIBRARY,distribution,Part_conv,A,B):
+    # take as input the number of bins (BINS), the diversity (Diversity) and size of the library sorted (N), the number of reads to allocate in total (BUDGET_READS), the post sorting amplification step (Ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Part_conv),and the parameters of the said distribution.
+    # Return the (Diversity*Bins) matrix resulting from the sequencing
+    global Sij
+    #### STEP 1 - Draw the ratio p_concentration
+    
+    def sorting_protein_matrix_populate(i,j):
+        return(stats.norm.cdf(Part_conv[j+1],loc=A[i], scale=B[i])-stats.norm.cdf(Part_conv[j],loc=A[i], scale=B[i]))
+
+    if BIAS_LIBRARY==True:
+       params=np.ones(Diversity)
+       Dir=[random.gammavariate(a,1) for a in params]
+       Dir=[v/sum(Dir) for v in Dir]
+       # Sample from the 30,000 simplex to get ratios
+       #p_concentration=np.ones(Diversity)/Diversity
+       p_concentration=Dir
+    else:
+       p_concentration=[1/Diversity]*Diversity
+
+    #### STEP 2 - Draw the sample sizes= of each genetic construct
+
+    Ni=np.random.multinomial(N, p_concentration, size=1)[0]
+    #Ni=Ni[0]
+
+    #### STEP 3 - Compute binning
+
+    ## Compute ratios qji
+    Qij=np.fromfunction(sorting_protein_matrix_populate, (Diversity, BINS), dtype=int)
+    #Qij=np.fromfunction(lambda i, j: i + j, (Diversity, BINS), dtype=int)
+    ## Compute Nij
+    Nij=Qij* Ni[:, np.newaxis]
+    Nij=np.floor(Nij) #Convert to Integer numbers
+
+    #### STEP 4 - PCR amplification
+
+    Nij_amplified=np.multiply(Nij,Ratio_amplification)
+
+    #### STEP 5 - Compute Reads allocation
+    N=np.sum(Nij)
+    Nj=np.sum(Nij, axis=0)
+    READS=np.floor(Nj*BUDGET_READS/N) #Allocate reads with repsect to the number of cells srted in each bin
+    #### STEP 6 - DNA sampling
+
+    Sij=np.zeros((Diversity,BINS))
+
+    #Compute ratios& Multinomial sampling
+    for j in range(BINS):
+        if np.sum(Nij_amplified,axis=0)[j]!=0:
+            Concentration_vector=Nij_amplified[:,j]/np.sum(Nij_amplified,axis=0)[j]
+        else:
+            Concentration_vector=np.zeros(Diversity)
+        Sij[:,j]=np.random.multinomial(READS[j],Concentration_vector,size=1)
+    print(Sij)
+
+
+
+def Sorting(BINS,Diversity,N,BIAS_LIBRARY,distribution,Part_conv,A,B):
+# take as input the number of bins (BINS), the diversity (Diversity) and size of the library sorted (N), the post sorting amplification step (Ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Part_conv),and the parameters of the said distribution.
+# Return the (Diversity*Bins) matrix resulting from the sorting step
+
+    global Sij
+    #### STEP 1 - Draw the ratio p_concentration
+    
+    def sorting_protein_matrix_populate(i,j):
+        return(stats.norm.cdf(Part_conv[j+1],loc=A[i], scale=B[i])-stats.norm.cdf(Part_conv[j],loc=A[i], scale=B[i]))
+
+    if BIAS_LIBRARY==True:
+       params=np.ones(Diversity)
+       Dir=[random.gammavariate(a,1) for a in params]
+       Dir=[v/sum(Dir) for v in Dir]
+       # Sample from the 30,000 simplex to get ratios
+       #p_concentration=np.ones(Diversity)/Diversity
+       p_concentration=Dir
+    else:
+       p_concentration=[1/Diversity]*Diversity
+
+    #### STEP 2 - Draw the sample sizes= of each genetic construct
+
+    Ni=np.random.multinomial(N, p_concentration, size=1)[0]
+    #Ni=Ni[0]
+
+    #### STEP 3 - Compute binning
+
+    ## Compute ratios qji
+    Qij=np.fromfunction(sorting_protein_matrix_populate, (Diversity, BINS), dtype=int)
+    #Qij=np.fromfunction(lambda i, j: i + j, (Diversity, BINS), dtype=int)
+    ## Compute Nij
+    Nij=Qij* Ni[:, np.newaxis]
+    Nij=np.floor(Nij) #Convert to Integer numbers
+    return(Nij)
+    
+    
+def Sequencing(BINS,Diversity,BUDGET_READS,Ratio_amplification,Nij):
+# take as input the number of bins (BINS), the diversity (Diversity) and size of the library sorted (N), the number of reads to allocate in total (BUDGET_READS), the post sorting amplification step (Ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Part_conv),and the parameters of the said distribution.
+# Return the (Diversity*Bins) matrix resulting from the sequencing
+
+    #### STEP 4 - PCR amplification
+
+    Nij_amplified=np.multiply(Nij,Ratio_amplification)
+
+    #### STEP 5 - Compute Reads allocation
+    N=np.sum(Nij)
+    Nj=np.sum(Nij, axis=0)
+    READS=np.floor(Nj*BUDGET_READS/N) #Allocate reads with repsect to the number of cells srted in each bin
+    #### STEP 6 - DNA sampling
+
+    Sij=np.zeros((Diversity,BINS))
+
+    #Compute ratios& Multinomial sampling
+    for j in range(BINS):
+        if np.sum(Nij_amplified,axis=0)[j]!=0:
+            Concentration_vector=Nij_amplified[:,j]/np.sum(Nij_amplified,axis=0)[j]
+        else:
+            Concentration_vector=np.zeros(Diversity)
+        Sij[:,j]=np.random.multinomial(READS[j],Concentration_vector,size=1)
+    return(Sij)
