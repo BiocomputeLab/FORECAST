ab3e927fc596fe1c095f5b63a8df0908b47f33ec
Pierre-Aurelien Gilliot <66125433+Pierre-Aurelien@users.noreply.github.com>
2020-12-01 17:30:17 +0000

    Created simulation class

--- flair/simulations.py
+++ flair/simulations.py
@@ -12,37 +12,63 @@ from joblib import Parallel, delayed
 
 
 
-def Sorting_and_Sequencing(BINS,Diversity,N,BUDGET_READS,Ratio_amplification,BIAS_LIBRARY,distribution,Part_conv,A,B):
-    # take as input the number of bins (BINS), the diversity (Diversity) and size of the library sorted (N), the number of reads to allocate in total (BUDGET_READS), the post sorting amplification step (Ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Part_conv),and the parameters of the said distribution.
-    # Return the (Diversity*Bins) matrix resulting from the sequencing and the sorting matrix Nj (number of cell sorted in each bin)
+class Simulation():
+    def __init__(self,bins,diversity,size,reads,fmax,distribution,ratio_amplification,theta1,theta2,bias_library):
+        self.bins=bins
+        self.diversity=diversity
+        self.size=size
+        self.reads=reads #number of reads in total for your simulation
+        self.fmax=fmax
+        self.distribution=distribution
+        if distribution=='lognormal':
+            # Working in log-space 
+            self.partitioning=np.log(np.logspace(0,np.log10(self.fmax),bins+1))
+        else:
+            partitioning=np.logspace(0,np.log10(self.fmax),bins+1)
+            partitioning[0]=0
+            self.partitioning=partitioning
+        self.ratio_amplification=ratio_amplification #post sorting PCR: What is the PCR amplification ratio?
+        self.theta1=theta1 #first parameter of the distribution (mu for a normal distirbution or shape for a gamma distribution)
+        self.theta2=theta2 #second parameter of the distribution (sigma for a normal distribution or scale for a gamma distribution)
+        self.bias_library=bias_library
+
+
+
+
+
+
+
+def Sorting_and_Sequencing(Simulation):
+    # take as input the number of bins (Simulation.bins), the diversity (Simulation.diversity) and size of the library sorted (N), the number of reads to allocate in total (Simulation.reads), the post sorting amplification step (Simulation.ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein Simulation.distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Simulation.partitioning),and the parameters of the said Simulation.distribution.
+    # Return the (Simulation.diversity*Bins) matrix resulting from the sequencing and the sorting matrix Nj (number of cell sorted in each bin)
     global Sij
     #### STEP 1 - Draw the ratio p_concentration
     
     def sorting_protein_matrix_populate(i,j):
-        if distribution=='lognormal':
-            element_matrix=stats.norm.cdf(Part_conv[j+1],loc=A[i], scale=B[i])-stats.norm.cdf(Part_conv[j],loc=A[i], scale=B[i])
+        if Simulation.distribution=='lognormal':
+            element_matrix=stats.norm.cdf(Simulation.partitioning[j+1],loc=Simulation.theta1[i], scale=Simulation.theta2[i])-stats.norm.cdf(Simulation.partitioning[j],loc=Simulation.theta1[i], scale=Simulation.theta2[i])
         else:
-            element_matrix=stats.gamma.cdf(Part_conv[j+1],a=A[i], scale=B[i])-stats.gamma.cdf(Part_conv[j],a=A[i], scale=B[i])
+            element_matrix=stats.gamma.cdf(Simulation.partitioning[j+1],a=Simulation.theta1[i], scale=Simulation.theta2[i])-stats.gamma.cdf(Simulation.partitioning[j],a=Simulation.theta1[i], scale=Simulation.theta2[i])
         return(element_matrix)
 
-    if BIAS_LIBRARY==True:
-       params=np.ones(Diversity)
+    if Simulation.bias_library==True:
+       params=np.ones(Simulation.diversity)
        Dir=[random.gammavariate(a,1) for a in params]
        Dir=[v/sum(Dir) for v in Dir]
        # Sample from the 30,000 simplex to get ratios
-       #p_concentration=np.ones(Diversity)/Diversity
+       #p_concentration=np.ones(Simulation.diversity)/Simulation.diversity
        p_concentration=Dir
     else:
-       p_concentration=[1/Diversity]*Diversity
+       p_concentration=[1/Simulation.diversity]*Simulation.diversity
 
     #### STEP 2 - Draw the sample sizes= of each genetic construct
 
-    Ni=np.random.multinomial(N, p_concentration, size=1)[0]
+    Ni=np.random.multinomial(Simulation.size, p_concentration, size=1)[0]
 
     #### STEP 3 - Compute binning
 
     ## Compute ratios qji
-    Qij=np.fromfunction(sorting_protein_matrix_populate, (Diversity, BINS), dtype=int)
+    Qij=np.fromfunction(sorting_protein_matrix_populate, (Simulation.diversity, Simulation.bins), dtype=int)
 
     ## Compute Nij
     Nij=Qij* Ni[:, np.newaxis]
@@ -50,88 +76,88 @@ def Sorting_and_Sequencing(BINS,Diversity,N,BUDGET_READS,Ratio_amplification,BIA
 
     #### STEP 4 - PCR amplification
 
-    Nij_amplified=np.multiply(Nij,Ratio_amplification)
+    Nij_amplified=np.multiply(Nij,Simulation.ratio_amplification)
 
     #### STEP 5 - Compute Reads allocation
     N=np.sum(Nij)
     Nj=np.sum(Nij, axis=0)
-    READS=np.floor(Nj*BUDGET_READS/N) #Allocate reads with repsect to the number of cells srted in each bin
+    READS=np.floor(Nj*Simulation.reads/N) #Allocate reads with repsect to the number of cells srted in each bin
     #### STEP 6 - DNA sampling
 
-    Sij=np.zeros((Diversity,BINS))
+    Sij=np.zeros((Simulation.diversity,Simulation.bins))
 
     #Compute ratios& Multinomial sampling
-    for j in range(BINS):
+    for j in range(Simulation.bins):
         if np.sum(Nij_amplified,axis=0)[j]!=0:
             Concentration_vector=Nij_amplified[:,j]/np.sum(Nij_amplified,axis=0)[j]
         else:
-            Concentration_vector=np.zeros(Diversity)
+            Concentration_vector=np.zeros(Simulation.diversity)
         Sij[:,j]=np.random.multinomial(READS[j],Concentration_vector,size=1)
     return(Sij,Nj)
 
 
 
-def Sorting(BINS,Diversity,N,BIAS_LIBRARY,distribution,Part_conv,A,B):
-# take as input the number of bins (BINS), the diversity (Diversity) and size of the library sorted (N), the post sorting amplification step (Ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Part_conv),and the parameters of the said distribution.
-# Return the (Diversity*Bins) matrix resulting from the sorting step
+def Sorting(Simulation):
+# take as input the number of bins (Simulation.bins), the diversity (Simulation.diversity) and size of the library sorted (N), the post sorting amplification step (Simulation.ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein Simulation.distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Simulation.partitioning),and the parameters of the said Simulation.distribution.
+# Return the (Simulation.diversity*Bins) matrix resulting from the sorting step
 
     global Sij
     #### STEP 1 - Draw the ratio p_concentration
     
     def sorting_protein_matrix_populate(i,j):
-        if distribution=='lognormal':
-            element_matrix=stats.norm.cdf(Part_conv[j+1],loc=A[i], scale=B[i])-stats.norm.cdf(Part_conv[j],loc=A[i], scale=B[i])
+        if Simulation.distribution=='lognormal':
+            element_matrix=stats.norm.cdf(Simulation.partitioning[j+1],loc=Simulation.theta1[i], scale=Simulation.theta2[i])-stats.norm.cdf(Simulation.partitioning[j],loc=Simulation.theta1[i], scale=Simulation.theta2[i])
         else:
-            element_matrix=stats.gamma.cdf(Part_conv[j+1],a=A[i], scale=B[i])-stats.gamma.cdf(Part_conv[j],a=A[i], scale=B[i])
+            element_matrix=stats.gamma.cdf(Simulation.partitioning[j+1],a=Simulation.theta1[i], scale=Simulation.theta2[i])-stats.gamma.cdf(Simulation.partitioning[j],a=Simulation.theta1[i], scale=Simulation.theta2[i])
         return(element_matrix)
 
-    if BIAS_LIBRARY==True:
-       params=np.ones(Diversity)
+    if Simualtion.bias_library==True:
+       params=np.ones(Simulation.diversity)
        Dir=[random.gammavariate(a,1) for a in params]
        Dir=[v/sum(Dir) for v in Dir]
        # Sample from the 30,000 simplex to get ratios
-       #p_concentration=np.ones(Diversity)/Diversity
+       #p_concentration=np.ones(Simulation.diversity)/Simulation.diversity
        p_concentration=Dir
     else:
-       p_concentration=[1/Diversity]*Diversity
+       p_concentration=[1/Simulation.diversity]*Simulation.diversity
 
     #### STEP 2 - Draw the sample sizes= of each genetic construct
 
-    Ni=np.random.multinomial(N, p_concentration, size=1)[0]
+    Ni=np.random.multinomial(Simulation.size, p_concentration, size=1)[0]
     #Ni=Ni[0]
 
     #### STEP 3 - Compute binning
 
     ## Compute ratios qji
-    Qij=np.fromfunction(sorting_protein_matrix_populate, (Diversity, BINS), dtype=int)
-    #Qij=np.fromfunction(lambda i, j: i + j, (Diversity, BINS), dtype=int)
+    Qij=np.fromfunction(sorting_protein_matrix_populate, (Simulation.diversity, Simulation.bins), dtype=int)
+
     ## Compute Nij
     Nij=Qij* Ni[:, np.newaxis]
     Nij=np.floor(Nij) #Convert to Integer numbers
     return(Nij)
     
     
-def Sequencing(BINS,Diversity,BUDGET_READS,Ratio_amplification,Nij):
-# take as input the number of bins (BINS), the diversity (Diversity) and size of the library sorted (N), the number of reads to allocate in total (BUDGET_READS), the post sorting amplification step (Ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Part_conv),and the parameters of the said distribution.
-# Return the (Diversity*Bins) matrix resulting from the sequencing Sij and the sorting matrix Nj (number of cell sorted in each bin)
+def Sequencing(Simulation,Nij):
+# take as input the number of bins (Simulation.bins), the diversity (Simulation.diversity) and size of the library sorted (N), the number of reads to allocate in total (Simulation.reads), the post sorting amplification step (Simulation.ratio_amplification),if the library is balanced (BIAS_Library), the underlying protein Simulation.distribution (gamma or lognormal), the fluorescence bounds for the sorting machine (Simulation.partitioning),and the parameters of the said Simulation.distribution.
+# Return the (Simulation.diversity*Bins) matrix resulting from the sequencing Sij and the sorting matrix Nj (number of cell sorted in each bin)
 
     #### STEP 4 - PCR amplification
 
-    Nij_amplified=np.multiply(Nij,Ratio_amplification)
+    Nij_amplified=np.multiply(Nij,Simulation.ratio_amplification)
 
     #### STEP 5 - Compute Reads allocation
     N=np.sum(Nij)
     Nj=np.sum(Nij, axis=0)
-    READS=np.floor(Nj*BUDGET_READS/N) #Allocate reads with repsect to the number of cells srted in each bin
+    READS=np.floor(Nj*Simulation.reads/N) #Allocate reads with repsect to the number of cells srted in each bin
     #### STEP 6 - DNA sampling
 
-    Sij=np.zeros((Diversity,BINS))
+    Sij=np.zeros((Simulation.diversity,Simulation.bins))
 
     #Compute ratios& Multinomial sampling
-    for j in range(BINS):
+    for j in range(Simulation.bins):
         if np.sum(Nij_amplified,axis=0)[j]!=0:
             Concentration_vector=Nij_amplified[:,j]/np.sum(Nij_amplified,axis=0)[j]
         else:
-            Concentration_vector=np.zeros(Diversity)
+            Concentration_vector=np.zeros(Simulation.diversity)
         Sij[:,j]=np.random.multinomial(READS[j],Concentration_vector,size=1)
     return(Sij,Nj)
